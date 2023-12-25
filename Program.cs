768893f70a71c4c09ccca40f36722c58c3dddae2
Matous Bocek <matous.bocek@deltatre.com>
2023-12-25 14:12:47 +0100

    added bug predicatble salt

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var password = "password";
+var salt = Encoding.UTF8.GetBytes("salty");
+var hashed = new Rfc2898DeriveBytes(password, salt); // Noncompliant code
